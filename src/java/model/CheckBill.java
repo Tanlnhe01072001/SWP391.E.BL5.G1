4ab8cdf2442af4d334b10c9ee69ac3abad696d94
Nhat Tan <tanln01072001@gmail.com>
2024-11-29 21:03:22 +0700

    update model

---
+++ src/java/model/CheckBill.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ThangNPHE151263
+ */
+public class CheckBill {
+
+    User user;
+    Cart cart;
+    String payment;
+    String address;
+    String phone;
+    double totalAmount;
+    String couponCode;
+
+    public CheckBill() {
+    }
+
+    public CheckBill(User user, Cart cart, String payment, String address, String phone) {
+        this.user = user;
+        this.cart = cart;
+        this.payment = payment;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
+    }
+}
---
+++ src/java/model/CouponType.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author ThangNPHE151263
+ */
+public class CouponType {
+    private int couponTypeId;
+    private String couponDescription;
+    private BigDecimal discountAmount;
+
+   public CouponType(int couponTypeId, String couponDescription, BigDecimal discountAmount) {
+        this.couponTypeId = couponTypeId;
+        this.couponDescription = couponDescription;
+        this.discountAmount = discountAmount;
+    }
+
+    public int getCouponTypeId() {
+        return couponTypeId;
+    }
+
+    public void setCouponTypeId(int couponTypeId) {
+        this.couponTypeId = couponTypeId;
+    }
+
+    public String getCouponDescription() {
+        return couponDescription;
+    }
+
+    public void setCouponDescription(String couponDescription) {
+        this.couponDescription = couponDescription;
+    }
+
+    public BigDecimal getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(BigDecimal discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+}
