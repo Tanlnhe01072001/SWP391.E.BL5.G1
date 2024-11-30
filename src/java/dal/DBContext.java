b04e8eddd37b932f90e5540551067d779480e5f7
Nhat Tan <tanln01072001@gmail.com>
2024-11-30 15:15:39 +0700

    update Admin manager

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -31,7 +31,7 @@ public class DBContext {
     private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
 
-    private final String password = "123456";
+    private final String password = "1";
      
 }
 
